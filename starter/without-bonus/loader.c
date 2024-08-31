421a75e23e62ee8b4fce106329d9056ce664f589
Gunika505 <162486134+Gunika505@users.noreply.github.com>
2024-09-01 02:59:16 +0530

    Update loader.c

--- starter/without-bonus/loader.c
+++ starter/without-bonus/loader.c
@@ -14,18 +14,50 @@ void loader_cleanup() {
 /*
  * Load and run the ELF executable file
  */
-void load_and_run_elf(char** exe) {
-  fd = open(argv[1], O_RDONLY);
-  // 1. Load entire binary content into the memory from the ELF file.
-  // 2. Iterate through the PHDR table and find the section of PT_LOAD 
-  //    type that contains the address of the entrypoint method in fib.c
+void load_and_run_elf(char* exe) {
+// 1. Load entire binary content into the memory from the ELF file.
+    filedes=open(exe, O_RDONLY);
+    off_t fdsize=lseek(fd,0,SEEK_END);
+    //error handling
+    if (size==-1){
+        printf("error in file size\n");
+        exit(1);
+    }
+    lseek(fd,0,SEEK_SET);
+    //entire file
+    char* heapmemalloc=(char*)malloc(fdsize);
+    //error handling
+    if (!heapmemalloc){
+        printf("error in memory allocation\n");
+        exit(1);
+    }
+    ssize_t readfile=read(fd, heapmemalloc, fdsize);
+
+    //error handling
+    if (readfile<0 || (size_t)readfile!=fd_size){
+    perror("error in reading file");
+    free(heapmemalloc);
+    exit(1);
+    }
+    ehdr=(Elf32_Ehdr*)heapmemalloc;
+    phdr=(Elf32_Ehdr*)(heapmemalloc+(*ehdr).e_phoff);
+
+// 2. Iterate through the PHDR table and find the section of PT_LOAD type that contains the address of the entrypoint method in fib.c
+    unsigned int epaddress=(*ehdr).e_entry;
+    int phdrsize=(*ehdr).e_phentsize;
+    int phnum=(*ehdr).e_phnum;
+    for (int i=0;i<phnum;i++){
+        //plsplspls figure out
+    }
+
   // 3. Allocate memory of the size "p_memsz" using mmap function 
   //    and then copy the segment content
   // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
   // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
   // 6. Call the "_start" method and print the value returned from the "_start"
-  int result = _start();
-  printf("User _start return value = %d\n",result);
+    int result = _start();
+    printf("User _start return value = %d\n",result);
+
 }
 
 int main(int argc, char** argv) 
