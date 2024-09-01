b54d8a2f729ce02e9894f00ef5774a2022051776
KRanaa <162487032+KRanaa@users.noreply.github.com>
2024-09-01 20:37:29 +0530

    Update loader.c

--- starter/without-bonus/loader.c
+++ starter/without-bonus/loader.c
@@ -46,9 +46,39 @@ void load_and_run_elf(char* exe) {
     unsigned int epaddress=(*ehdr).e_entry;
     int phdrsize=(*ehdr).e_phentsize;
     int phnum=(*ehdr).e_phnum;
+
+  // 3. Allocate memory of the size "p_memsz" using mmap function 
+        //    and then copy the segment content
     for (int i=0;i<phnum;i++){
-        //plsplspls figure out
+        if(phdr[i].p_type==PT_LOAD){
+            void *segmem=mmap((void *)phdr[i].p_vaddr, phdr[i].p_memsz,
+                                PROT_READ | PROT_WRITE | PROT_EXEC,
+                                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+            
+            if (segmem == MAP_FAILED) {
+                perror("Error in mmap");
+                free(heapmemalloc);
+                exit(1);
+            }
+
+            memcpy(segmem, heapmemalloc + phdr[i].p_offset, phdr[i].p_filesz);
+
+            if (phdr[i].p_memsz > phdr[i].p_filesz) {
+                memset(segmem + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);
+            }
+        
+        }
     }
+
+    // 5. Typecast the entry point address to a function pointer
+    void (*entry_point)();
+    entry_point = (void (*)())epaddress;
+
+    // 6. Call the entry point (typically "_start" in an ELF file)
+    entry_point();  // Jump to the entry point and start execution
+
+    free(heapmemalloc);  // Free the allocated memory
+}
 
   // 3. Allocate memory of the size "p_memsz" using mmap function 
   //    and then copy the segment content
